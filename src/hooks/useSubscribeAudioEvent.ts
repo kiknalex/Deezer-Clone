d924d32c3dcb1aa3fc73b389b071606ea2360f94
Alex <aleksandrekiknadzee@gmail.com>
2024-06-14 18:03:38 +0400

    feat: add useCallback to useSubscribeAudioEvent hook

--- src/hooks/useSubscribeAudioEvent.ts
+++ src/hooks/useSubscribeAudioEvent.ts
@@ -1,25 +1,24 @@
-import { useSyncExternalStore } from "react";
+import { useCallback, useSyncExternalStore } from "react";
 
-const useSubscribeAudioEvents = (
+const useSubscribeAudioEvent = (
   audioRef: React.RefObject<HTMLAudioElement>,
   callback: () => void,
   eventName: keyof HTMLElementEventMap
 ) => {
-  const subscribe = () => {
+  const subscribe = useCallback(() => {
     const audioElement = audioRef.current;
     if (!audioElement) return () => {};
-
     audioElement.addEventListener(eventName, callback);
     return () => {
       audioElement.removeEventListener(eventName, callback);
     };
-  };
+  }, [audioRef, eventName, callback]);
 
-  const getSnapshot = () => {
+  const getSnapshot = useCallback(() => {
     return audioRef.current;
-  };
+  }, [audioRef]);
 
   return useSyncExternalStore(subscribe, getSnapshot);
 };
 
-export default useSubscribeAudioEvents;
+export default useSubscribeAudioEvent;

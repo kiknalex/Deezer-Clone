ac823e41b65ff88cddc4ff5057cef861dc4be2c8
Alex <aleksandrekiknadzee@gmail.com>
2024-06-20 13:07:13 +0400

    refactor: changee name of the subscribe custom hook

---
+++ src/hooks/useSubscribeBrowserEvent.ts
@@ -0,0 +1,24 @@
+import { useCallback, useSyncExternalStore } from "react";
+
+const useSubscribeBrowserEvent = (
+  ref: React.RefObject<HTMLAudioElement>,
+  callback: () => void,
+  eventName: keyof HTMLElementEventMap
+) => {
+  const subscribe = useCallback(() => {
+    const element = ref.current;
+    if (!element) return () => {};
+    element.addEventListener(eventName, callback);
+    return () => {
+      element.removeEventListener(eventName, callback);
+    };
+  }, [ref, eventName, callback]);
+
+  const getSnapshot = useCallback(() => {
+    return ref.current;
+  }, [ref]);
+
+  return useSyncExternalStore(subscribe, getSnapshot);
+};
+
+export default useSubscribeBrowserEvent;

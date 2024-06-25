b55cd4ef47a40f2598fed509ca4704d757958ebd
Alex <aleksandrekiknadzee@gmail.com>
2024-06-25 18:43:46 +0400

    feat: add MainPage styles

---
+++ src/components/main-page/Mainpage.css.ts
@@ -0,0 +1,7 @@
+import { style } from "@vanilla-extract/css";
+
+export const mainLayout = style({
+  paddingTop: "var(--header-height)",
+  paddingBottom: "var(--player-height)",
+  height: "100vh",
+});

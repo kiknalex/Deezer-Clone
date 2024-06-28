ff4d243e7a7fab03254afda24cafd4ea8e38ea33
Alex <aleksandrekiknadzee@gmail.com>
2024-06-28 13:14:03 +0400

    feat: add responsive carousel

--- src/components/util-components/carousel/Carousel.css.ts
+++ src/components/util-components/carousel/Carousel.css.ts
@@ -1,22 +1,43 @@
+import { mediaSize } from "@/styles/sizes.css";
 import { style } from "@vanilla-extract/css";
 
 export const carouselStyle = style({
-  position: "relative",
   overflow: "hidden",
   width: "100%",
+  minWidth: "600px",
+  vars: { "--carousel-gap": "24px" },
+  "@media": {
+    [`screen and (min-width: ${mediaSize["size-xs"]})`]: {},
+    [`screen and (min-width: ${mediaSize["size-sm"]})`]: {
+      vars: { "--carousel-gap": "34px" },
+    },
+    [`screen and (min-width: ${mediaSize["size-md"]})`]: {},
+    [`screen and (min-width: ${mediaSize["size-lg"]})`]: {},
+    [`screen and (min-width: ${mediaSize["size-xl"]})`]: {},
+    [`screen and (min-width: ${mediaSize["size-xxl"]})`]: {},
+  },
 });
 
 export const carouselInner = style({
   width: "100%",
-  transition: "transform 0.5s ease",
+  transition: "transform 0.6s ease-in-out",
 });
 
 export const carouselList = style({
-  display: "flex",
+  width: `calc(100% + var(--carousel-gap))`,
+  display: "grid",
   overflow: "visible",
-  flexWrap: "nowrap"
+  gridAutoColumns: `calc(25% - var(--carousel-gap))`,
+  gridAutoFlow: "column",
+  columnGap: "var(--carousel-gap)",
+  "@media": {
+    [`screen and (min-width: ${mediaSize["size-xs"]})`]: {},
+    [`screen and (min-width: ${mediaSize["size-sm"]})`]: {
+    },
+    [`screen and (min-width: ${mediaSize["size-md"]})`]: {},
+    [`screen and (min-width: ${mediaSize["size-lg"]})`]: {},
+    [`screen and (min-width: ${mediaSize["size-xl"]})`]: {},
+    [`screen and (min-width: ${mediaSize["size-xxl"]})`]: {gridAutoColumns: "calc(20% - var(--carousel-gap))"},
+  },
 });
-
-export const slideStyle = style({
-  minWidth: "25%",
-});
+export const slideStyle = style({});
--- src/components/util-components/carousel/Carousel.tsx
+++ src/components/util-components/carousel/Carousel.tsx
@@ -1,7 +1,16 @@
-import { useState } from 'react';
-import { carouselInner, carouselList, carouselStyle, slideStyle } from './Carousel.css';
+import { useState } from "react";
+import {
+  carouselInner,
+  carouselList,
+  carouselStyle,
+  slideStyle,
+  varGap,
+  
+} from "./Carousel.css";
+import { container } from "@/app/App.css";
+import { sprinkles } from "@/styles/sprinkles.css";
 
-const Carousel = ({ slides }) => {
+const Carousel = ({ slides, heading }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const nextSlide = () => {
@@ -13,18 +22,32 @@ const Carousel = ({ slides }) => {
   };
 
   return (
-    <div className={carouselStyle}>
-      <div className={carouselInner} style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
-        <ul className={carouselList}>
-        {slides.map((slide, index) => (
-          <li key={index} className={slideStyle}>
-            {slide}
-          </li>
-        ))}
-        </ul>
+    <div className={container}>
+      <div className={`${carouselStyle}`}>
+        <div className={sprinkles({display: "flex", justifyContent: "space-between"})}>
+          <h2>Heading</h2>
+          <div>
+          <button className="carousel-control prev" onClick={prevSlide}>
+            &#10094;
+          </button>
+          <button className="carousel-control next" onClick={nextSlide}>
+            &#10095;
+          </button>
+          </div>
+        </div>
+        <div
+          className={carouselInner}
+          style={{ transform: `translateX(calc(-${currentSlide * 100}% - calc(var(--carousel-gap) * ${currentSlide})))` }} // calculate translate: total width - grid gaps
+        >
+          <ul className={carouselList}>
+            {slides.map((slide, index) => (
+              <li key={index} className={slideStyle}>
+                {slide}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <button className="carousel-control prev" onClick={prevSlide}>&#10094;</button>
-      <button className="carousel-control next" onClick={nextSlide}>&#10095;</button>
     </div>
   );
 };

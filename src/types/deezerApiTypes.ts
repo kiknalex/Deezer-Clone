1ce9e1241561ad6ca507d0e65da31460180223e6
Alex <aleksandrekiknadzee@gmail.com>
2024-07-01 13:08:32 +0400

    feat: add genre data type

--- src/types/deezerApiTypes.ts
+++ src/types/deezerApiTypes.ts
@@ -78,3 +78,14 @@ export interface MusicData {
     type: string
   }
   
+  export interface Genre {
+    id: number;
+    name: string;
+    picture: string;
+    picture_small: string;
+    picture_medium: string;
+    picture_big: string;
+    picture_xl: string;
+    type: string;
+  }
+  

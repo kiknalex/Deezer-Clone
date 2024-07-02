aa5cb629a7b4262869bbb1b774db42ed1e9383d6
Alex <aleksandrekiknadzee@gmail.com>
2024-07-02 11:20:49 +0400

    feat: add PlaylistCard styles

--- src/components/main-page/playlist-card/PlaylistCard.css.ts
+++ src/components/main-page/playlist-card/PlaylistCard.css.ts
@@ -1,6 +1,19 @@
+import { linkHover } from "@/components/music-player/TrackPlaybackControls/TrackControls/TrackPreview/TrackPreview.css";
+import colors from "@/styles/colors.css";
+import { size } from "@/styles/sizes.css";
+import { fontSize } from "@/styles/typography.css";
 import { style } from "@vanilla-extract/css";
 
 export const cardImg = style({
   width: "100%",
   height: "auto",
+  borderRadius: "9px",
 });
+
+export const link = linkHover;
+
+export const tracksNumber = style({
+  fontSize: fontSize["font-size-1"],
+  color: colors["--gray-6"],
+  marginTop: size["size-1"]
+})

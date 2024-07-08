3b77470fd208ec70ef973a630d88304d4848a29d
Alex <aleksandrekiknadzee@gmail.com>
2024-07-08 17:03:39 +0400

    feat: ensure main page always fills the srceen

--- src/components/main-page/Mainpage.css.ts
+++ src/components/main-page/Mainpage.css.ts
@@ -2,6 +2,7 @@ import { vars } from "@/app/theme.css";
 import { style } from "@vanilla-extract/css";
 
 export const mainLayout = style({
+  minHeight: "calc(100vh - var(--header-height))",
     display: "block",
   paddingTop: "var(--header-height)",
   paddingBottom: "24px",

7d19badc280b4610bd8c87e07ff6e5a870f007de
Alex <aleksandrekiknadzee@gmail.com>
2024-07-17 18:34:08 +0400

    feat: add seconds to hours helper function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -11,3 +11,10 @@ export const debounce = (func: NoArgVoidFunction, wait: number): NoArgVoidFuncti
   export const encodeForURL = (text: string) => {
     return encodeURIComponent(text.toLowerCase().replace(/ /g, ''));
 }
+
+export const secondsToHours = (seconds: number) => {
+  if(seconds <= 3600) return `${Math.round(seconds / 60)} minutes`;
+
+  return `${Math.round(seconds / 3600)} hours ${Math.round(seconds / 60) % 60} minutes`
+
+}

1fb9b4fc30e8393456d62ed34b80492c66d24927
Alex <aleksandrekiknadzee@gmail.com>
2024-07-19 13:43:45 +0400

    feat: add unixToDate converter helper function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,20 +1,34 @@
 type NoArgVoidFunction = () => void;
 
-export const debounce = (func: NoArgVoidFunction, wait: number): NoArgVoidFunction => {
-    let timeout: NodeJS.Timeout;
-    return () => {
-      clearTimeout(timeout);
-      timeout = setTimeout(() => func(), wait);
-    };
+export const debounce = (
+  func: NoArgVoidFunction,
+  wait: number
+): NoArgVoidFunction => {
+  let timeout: NodeJS.Timeout;
+  return () => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(), wait);
   };
+};
 
-  export const encodeForURL = (text: string) => {
-    return encodeURIComponent(text.toLowerCase().replace(/ /g, ''));
-}
+export const encodeForURL = (text: string) => {
+  return encodeURIComponent(text.toLowerCase().replace(/ /g, ""));
+};
 
 export const secondsToHours = (seconds: number) => {
-  if(seconds <= 3600) return `${Math.round(seconds / 60)} minutes`;
+  if (seconds <= 3600) return `${Math.round(seconds / 60)} minutes`;
 
-  return `${Math.round(seconds / 3600)} hours ${Math.round(seconds / 60) % 60} minutes`
+  return `${Math.round(seconds / 3600)} hours ${
+    Math.round(seconds / 60) % 60
+  } minutes`;
+};
 
+export const unixToDateString = (timestamp: number) => {
+  const date = new Date(timestamp * 1000);
+  const options: Intl.DateTimeFormatOptions = {
+    month: "2-digit",
+    day: "2-digit",
+    year: "numeric",
+  }
+  return date.toLocaleDateString("en-US", options);
 }

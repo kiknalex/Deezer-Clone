e424d86b2fa41f17483b957ea205c88cfe2a2dff
Alex <aleksandrekiknadzee@gmail.com>
2024-07-22 18:39:05 +0400

    feat: add ScrollRestoration to the main page

--- src/components/main-page/Mainpage.tsx
+++ src/components/main-page/Mainpage.tsx
@@ -1,10 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import { mainLayout } from "./Mainpage.css";
 
 const Mainpage = () => {
   return (
     <main className={mainLayout}>
       <Outlet />
+      <ScrollRestoration />
     </main>
   );
 };

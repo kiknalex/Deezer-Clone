dc31b6ef0b6357ca61dcb0dbf35d7cddbe9ad5e6
Alex <aleksandrekiknadzee@gmail.com>
2024-07-23 16:32:21 +0400

    feat: save darkmode to localstorage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { app } from "./App.css";
 import { darkTheme, lightTheme, loggedIn } from "./theme.css";
 import Sidebar from "@/components/sidebar/Sidebar";
 import Header from "@/components/header/Header";
 import Mainpage from "@/components/main-page/Mainpage";
+import { useBeforeUnload } from "react-router-dom";
 const App = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState<boolean>(
+    () => localStorage.getItem("darkmode") === "true"
+  );
+  useBeforeUnload(
+    useCallback(() => {
+      localStorage.setItem("darkmode", JSON.stringify(darkMode));
+    }, [darkMode])
+  );
 
   const handleDarkModeClick = () => {
     setDarkMode(!darkMode);

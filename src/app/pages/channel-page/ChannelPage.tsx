ab8c6727d814878e1dd06e9815543980517c5345
Alex <aleksandrekiknadzee@gmail.com>
2024-07-24 17:08:03 +0400

    fix: hide section if data array is empty

--- src/app/pages/channel-page/ChannelPage.tsx
+++ src/app/pages/channel-page/ChannelPage.tsx
@@ -18,56 +18,57 @@ const ChannelPage = () => {
     <div>
       <Suspense fallback={<LoaderSpin />}>
         <Await resolve={data}>
-          {({
-            genreArtists,
-            editorialReleases,
-            chartPlaylists,
-            genreName,
-          }) => (
+          {({ genreArtists, editorialReleases, chartPlaylists, genreName }) => (
             <>
               <h1 className={container}>{genreName}</h1>
-              <section className={container}>
-                <Carousel
-                  heading="Playlists"
-                  slides={chartPlaylists.data.map((playlist: Playlist) => (
-                    <PlaylistCard
-                      imgSrc={playlist.picture_medium}
-                      title={playlist.title}
-                      numberOfTracks={playlist.nb_tracks}
-                      tracklist={playlist.tracklist}
-                      id={playlist.id}
-                    />
-                  ))}
-                />
-              </section>
-              <section className={container}>
-                <Carousel
-                  heading="Artists"
-                  slides={genreArtists.data.map((artist: Artist) => (
-                    <ArtistCard
-                      imgSrc={artist.picture_medium}
-                      title={artist.name}
-                      tracklist={artist.tracklist}
-                      id={artist.id}
-                    />
-                  ))}
-                />
-              </section>
-              <section className={container}>
-                <Carousel
-                  heading="Editorial Releases"
-                  slides={editorialReleases.data.map((release: Release) => (
-                    <AlbumCard
-                      imgSrc={release.cover_medium}
-                      title={release.title}
-                      artist={release.artist}
-                      releaseDate={release.release_date}
-                      tracklist={release.tracklist}
-                      id={release.id}
-                    />
-                  ))}
-                />
-              </section>
+              {chartPlaylists.data.length > 0 && (
+                <section className={container}>
+                  <Carousel
+                    heading="Playlists"
+                    slides={chartPlaylists.data.map((playlist: Playlist) => (
+                      <PlaylistCard
+                        imgSrc={playlist.picture_medium}
+                        title={playlist.title}
+                        numberOfTracks={playlist.nb_tracks}
+                        tracklist={playlist.tracklist}
+                        id={playlist.id}
+                      />
+                    ))}
+                  />
+                </section>
+              )}
+              {genreArtists.data.length > 0 && (
+                <section className={container}>
+                  <Carousel
+                    heading="Artists"
+                    slides={genreArtists.data.map((artist: Artist) => (
+                      <ArtistCard
+                        imgSrc={artist.picture_medium}
+                        title={artist.name}
+                        tracklist={artist.tracklist}
+                        id={artist.id}
+                      />
+                    ))}
+                  />
+                </section>
+              )}
+              {editorialReleases.data.length > 0 && (
+                <section className={container}>
+                  <Carousel
+                    heading="Editorial Releases"
+                    slides={editorialReleases.data.map((release: Release) => (
+                      <AlbumCard
+                        imgSrc={release.cover_medium}
+                        title={release.title}
+                        artist={release.artist}
+                        releaseDate={release.release_date}
+                        tracklist={release.tracklist}
+                        id={release.id}
+                      />
+                    ))}
+                  />
+                </section>
+              )}
             </>
           )}
         </Await>
